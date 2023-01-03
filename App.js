30ddec0bfbcd5fa3d0668a39d4302ec60c688643
hyunjee <107829027+kimhyunjee@users.noreply.github.com>
2023-01-03 20:47:40 +0900

    react native draft

--- App.js
+++ App.js
@@ -1,11 +1,17 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Button } from "react-native";
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <View>
+        <Text style={styles.dummyText}>여기는 연습용i</Text>
+      </View>
+      <Text
+        style={{ margin: 16, borderWidth: 2, borderColor: "red", padding: 16 }}
+      >
+        다른 텍스트
+      </Text>
+      <Button title="tap me" />
     </View>
   );
 }
@@ -13,8 +19,14 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  dummyText: {
+    margin: 16,
+    padding: 16,
+    borderWidth: 16,
+    borderBottomColor: "red",
   },
 });
