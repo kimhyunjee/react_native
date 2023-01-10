c171014761b0bed91cea8db7568c9a239439b844
hyunjee <107829027+kimhyunjee@users.noreply.github.com>
2023-01-10 23:06:07 +0900

    scrollView

--- App.js
+++ App.js
@@ -1,14 +1,58 @@
-import { StyleSheet, Text, View, Button, TextInput } from "react-native";
+import {
+  StyleSheet,
+  Text,
+  View,
+  Button,
+  TextInput,
+  ScrollView,
+  FlatList,
+} from "react-native";
+import { useState } from "react";
 
 export default function App() {
+  const [enteredGoalText, setEnteredGoalText] = useState("");
+  const [courseGoals, setCourseGoals] = useState([]);
+
+  function goalInputHandler(enteredText) {
+    // console.log(enteredText); 아래 터미널에 한글자마다 입력됨
+    setEnteredGoalText(enteredText);
+  }
+
+  function addGoalHandler() {
+    // console.log(enteredGoalText);
+    setCourseGoals((currentCourseGoals) => [...courseGoals, enteredGoalText]);
+  }
+
   return (
     <View style={styles.appContainer}>
       <View style={styles.inputContainer}>
-        <TextInput style={styles.textInput} placeholder="your course goal! " />
-        <Button title="add goal" />
+        <TextInput
+          style={styles.textInput}
+          placeholder="검색어를 입력해주세요 "
+          onChangeText={goalInputHandler}
+        />
+        <Button title="검색하기" onPress={addGoalHandler} />
       </View>
       <View styles={styles.goalsContainer}>
-        <Text> List of goals...</Text>
+        {/* <ScrollView>
+          {courseGoals.map((goal) => (
+            <View key={goal} styles={styles.goalItem}>
+              <Text style={styles.goalText}>{goal}</Text>
+            </View>
+          ))}
+        </ScrollView> */}
+        <FlatList
+          data={courseGoals}
+          renderItem={(itemData) => {
+            // itemData는 index 프로퍼티 접근 권한도 제공한다
+            // itemData.index
+            return (
+              <View styles={styles.goalItem}>
+                <Text style={styles.goalText}>{itemData.item}</Text>
+              </View>
+            );
+          }}
+        />
       </View>
     </View>
   );
@@ -21,7 +65,7 @@ const styles = StyleSheet.create({
     paddingHorizontal: 16,
   },
   inputContainer: {
-    flex: 1,
+    flex: 0.5,
     flexDirection: "row",
     justifyContent: "space-between",
     alignItems: "center",
@@ -37,6 +81,26 @@ const styles = StyleSheet.create({
     padding: 8,
   },
   goalsContainer: {
-    flex: 5,
+    flex: 1,
+  },
+  category: {
+    margin: 16,
+    padding: 16,
+    borderWidth: 16,
+    borderBottomColor: "#f0f8ff",
+    flexDirection: "row",
+  },
+  goalItem: {
+    margin: 8,
+    padding: 8,
+    borderRadius: 6,
+    backgroundColor: "#5e0acc",
+  },
+  goalText: {
+    backgroundColor: "#5e0acc",
+    color: "white",
+    margin: 8,
+    padding: 8,
+    borderRadius: 6,
   },
 });
